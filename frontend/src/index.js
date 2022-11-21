90e16a24e67a3cce9aabe18c935278d2e64e2815
duncannah <38371914+duncannah@users.noreply.github.com>
2022-11-21 15:35:49 +0100

    feat: use scss with bootstrap

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,4 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './stylesheets/main.css';
+import './stylesheets/main.scss';
 
 import Navbar from './Components/Navbar/Navbar';
 import Router from './Components/Router/Router';
