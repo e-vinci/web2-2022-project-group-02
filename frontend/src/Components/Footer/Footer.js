c3bd10a5a7a36ce247f70b0db00ed8e6337d0e06
duncannah <38371914+duncannah@users.noreply.github.com>
2022-11-21 23:28:10 +0100

    feat: footer placeholder

---
+++ frontend/src/Components/Footer/Footer.js
@@ -0,0 +1,36 @@
+import html from '../../utils/html';
+import cat from '../../img/icons/cat.svg';
+
+const Footer = () => {
+  renderFooter();
+};
+
+function renderFooter() {
+  const footerWrapper = document.querySelector('body > footer');
+  const footer = html`
+    <div class="container">
+      <footer
+        class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top"
+      >
+        <p class="col-md-4 mb-0 text-muted">© 2022 CatByte</p>
+
+        <a
+          href="/"
+          class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none"
+        >
+          <svg viewBox="${cat.viewBox}" width="48" height="48">
+            <use xlink:href="#${cat.id}" />
+          </svg>
+        </a>
+
+        <ul class="nav col-md-4 justify-content-end">
+          <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">À propos</a></li>
+        </ul>
+      </footer>
+    </div>
+  `;
+
+  footerWrapper.replaceChildren(footer);
+}
+
+export default Footer;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,10 @@ import './stylesheets/main.scss';
 
 import Navbar from './Components/Navbar/Navbar';
 import Router from './Components/Router/Router';
+import Footer from './Components/Footer/Footer';
 
 Navbar();
 
 Router();
+
+Footer();
