59a302d2381b76b7207e059cd1dec46620487834
Sheraaa <chehrazad.ouazzani@student.vinci.be>
2022-11-28 23:44:08 +0100

    fixed errors in code

--- frontend/src/Components/Router/routes.js
+++ frontend/src/Components/Router/routes.js
@@ -2,11 +2,8 @@ import HomePage from '../Pages/HomePage';
 import Logout from '../Logout/Logout';
 import LoginPage from '../Pages/LoginPage';
 import RegisterPage from '../Pages/RegisterPage';
-<<<<<<< HEAD
 import ForumPage from '../Pages/ForumPage';
-=======
 import CoursesPage from '../Pages/CoursesPage';
->>>>>>> b2af11840eeb82631f2857f56e1f35562ae47090
 
 const routes = {
   '/': HomePage,
@@ -14,7 +11,7 @@ const routes = {
   '/login': LoginPage,
   '/register': RegisterPage,
   '/logout': Logout,
-  '/forum' : ForumPage,
+  '/forum': ForumPage,
 };
 
 export default routes;
