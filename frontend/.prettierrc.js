3bd6db98c780b11bc7db77191019d60243d78a3c
duncannah <38371914+duncannah@users.noreply.github.com>
2022-12-05 11:07:10 +0100

    fix: dependencies conflict

--- frontend/.prettierrc.js
+++ frontend/.prettierrc.js
@@ -1,5 +1,12 @@
 module.exports = {
-  ...require('prettier-airbnb-config'),
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  proseWrap: 'always',
+  quoteProps: 'as-needed',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
   printWidth: 100,
   arrowParens: 'always',
   bracketSpacing: true,
