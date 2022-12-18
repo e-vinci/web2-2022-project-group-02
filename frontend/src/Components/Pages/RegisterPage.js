406ccb1aab07c2d44b0c3d79eb4dd72194c5a3e5
duncannah <38371914+duncannah@users.noreply.github.com>
2022-12-18 18:56:56 +0100

    fix: better login and register

--- frontend/src/Components/Pages/RegisterPage.js
+++ frontend/src/Components/Pages/RegisterPage.js
@@ -21,30 +21,30 @@ function renderRegisterForm() {
 
   const form = html`
     <form class="p-5 form-box" id="register-form">
-      <input
-        type="email"
-        id="email"
-        placeholder="Adresse mail"
-        required
-        class="form-control mb-3"
-      />
-      <input type="text" id="username" placeholder="Pseudo" required class="form-control mb-3" />
-      <input
-        type="password"
-        id="password"
-        required
-        placeholder="Mot de passe"
-        class="form-control mb-3"
-      />
-      <input
-        type="password"
-        id="confirmPswd"
-        required
-        placeholder="Confirmer le mot de passe"
-        class="form-control mb-3"
-      />
-      <input type="checkbox" id="terms" required class="form-check-input mb-3" />
-      <label class="form-check-label mb-3">J'accepte ${termsLink}</label>
+      <div class="mb-3">
+        <label for="email" class="form-label">Adresse mail</label>
+        <input type="email" id="email" placeholder="" required class="form-control" />
+      </div>
+      <div class="mb-3">
+        <label for="username" class="form-label">Pseudo</label>
+        <input type="text" id="username" placeholder="" required class="form-control" />
+      </div>
+      <div class="mb-3">
+        <label for="password" class="form-label">Mot de passe</label>
+        <input type="password" id="password" required placeholder="" class="form-control" />
+        <div id="passwordHelpBlock" class="form-text">
+          Votre mot de passe doit comporter au minimum huit caractères, au moins une lettre
+          majuscule, une lettre minuscule, un chiffre, et un caractère spécial.
+        </div>
+      </div>
+      <div class="mb-3">
+        <label for="confirmPswd" class="form-label">Confirmer mot de passe</label>
+        <input type="password" id="confirmPswd" required placeholder="" class="form-control" />
+      </div>
+      <div class="mb-3">
+        <input type="checkbox" id="terms" required class="form-check-input " />
+        <label for="terms" class="form-check-label">J'accepte ${termsLink}</label>
+      </div>
       <div class="d-flex gap-3 justify-content-end">
         <div class="spinner-border d-none" role="status"></div>
         <input value="S'inscrire" type="submit" class="btn btn-info" />
@@ -52,9 +52,11 @@ function renderRegisterForm() {
     </form>
   `;
 
+  const el = html` <div class="container">${form}</div> `;
+
   form.addEventListener('submit', onRegister);
 
-  main.appendChild(form);
+  main.appendChild(el);
 }
 
 async function onRegister(e) {
