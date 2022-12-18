df637d631c5714c98e8a4658ff3a046939dc2ff6
duncannah <38371914+duncannah@users.noreply.github.com>
2022-12-18 22:08:27 +0100

    fix: friendly date plural

--- frontend/src/Components/FriendlyDate/FriendlyDate.js
+++ frontend/src/Components/FriendlyDate/FriendlyDate.js
@@ -1,6 +1,10 @@
 import { Tooltip as BootstrapTooltip } from 'bootstrap';
 import html from '../../utils/html';
 
+function pl(n) {
+  return `${n !== 1 ? 's' : ''}`;
+}
+
 export function getTimeAgo(_date) {
   let secondsAgo = Math.round((Date.now() - new Date(_date).getTime()) / 1000);
   const str = secondsAgo < 0 ? 'dans ' : 'il y a ';
@@ -11,25 +15,25 @@ export function getTimeAgo(_date) {
     secondsAgo *= -1;
   }
 
-  if (secondsAgo < 60) return `${str + secondsAgo} secondes`;
+  if (secondsAgo < 60) return `${str + secondsAgo} seconde${pl(secondsAgo)}`;
 
   const minutesAgo = Math.round(secondsAgo / 60);
-  if (minutesAgo < 60) return `${str + minutesAgo} minutes`;
+  if (minutesAgo < 60) return `${str + minutesAgo} minute${pl(minutesAgo)}`;
 
   const hoursAgo = Math.round(minutesAgo / 60);
-  if (hoursAgo < 24) return `${str + hoursAgo} heures`;
+  if (hoursAgo < 24) return `${str + hoursAgo} heure${pl(hoursAgo)}`;
 
   const daysAgo = Math.round(hoursAgo / 24);
-  if (daysAgo < 7) return `${str + daysAgo} jours`;
+  if (daysAgo < 7) return `${str + daysAgo} jour${pl(daysAgo)}`;
 
   const weeksAgo = Math.round(daysAgo / 7);
-  if (weeksAgo < 4) return `${str + weeksAgo} semaines`;
+  if (weeksAgo < 4) return `${str + weeksAgo} semaine${pl(weeksAgo)}`;
 
   const monthsAgo = Math.round(daysAgo / 30);
   if (monthsAgo < 12) return `${str + monthsAgo} mois`;
 
   const yearsAgo = Math.round(daysAgo / 365);
-  return `${str + yearsAgo} ans`;
+  return `${str + yearsAgo} an${pl(yearsAgo)}`;
 }
 
 export default function FriendlyDate(_date, dateType = 'full', timeType = 'medium') {
